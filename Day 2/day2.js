a3c87e7ba6b81335ab4da828554ad0ef54482857
Nathan <therealmoosee@gmail.com>
2023-12-02 15:59:37 -0500

    finished day 2

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -11,7 +11,7 @@ const readFileLines = filename =>
 
 let inputArray = readFileLines('input.txt');
 
-function main() {
+function part1() {
     let gameSum = 0;
 
     // Walk through all games
@@ -26,13 +26,17 @@ function main() {
 
             let violation = false;
             for (let k = 0; k < elementsInBag.length; k++) {
-              let number = elementsInBag[k].split(" ")[0]
+              let number = Number(elementsInBag[k].split(" ")[0])
               let text = elementsInBag[k].split(" ")[1]
 
-              switch(text) {
-                case "red": if (number > 12) {violation = true; break}
-                case "green": if (number > 13) {violation = true; break}
-                case "blue": if (number > 14) {violation = true; break}
+              if (text.includes("red")) {
+                if (number > 12) {violation = true; break}
+              }
+              else if (text.includes("green")) {
+                if (number > 13) {violation = true; break}
+              }
+              else if (text.includes("blue")) {
+                if (number > 14) {violation = true; break}
               }
             }
 
@@ -50,4 +54,41 @@ function main() {
     console.log(gameSum);
 }
 
-main()
+function part2() {
+  let gameSum = 0;
+
+  // Walk through all games
+  for (let i = 0; i < inputArray.length; i++) {
+      // Split this game into its individual bags
+      let listOfBags = inputArray[i].split(": ")[1].split("; ")
+      let redHighest = 0;
+      let greenHighest = 0;
+      let blueHighest = 0;
+
+      // Look through each bag and determine if that game is possible
+      for (let j = 0; j < listOfBags.length; j++) {
+          let elementsInBag = listOfBags[j].split(", ")
+
+          for (let k = 0; k < elementsInBag.length; k++) {
+            let number = Number(elementsInBag[k].split(" ")[0])
+            let text = elementsInBag[k].split(" ")[1]
+
+            if (text.includes("red")) {
+              if (number > redHighest) {redHighest = number; continue;}
+            }
+            else if (text.includes("green")) {
+              if (number > greenHighest) {greenHighest = number; continue;}
+            }
+            else if (text.includes("blue")) {
+              if (number > blueHighest) {blueHighest = number; continue;}
+            }
+          }
+      }
+
+      gameSum += redHighest * greenHighest * blueHighest
+  }
+
+  console.log(gameSum);
+}
+
+part2()
